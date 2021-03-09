87a7fd2ad0e85c5750e943347a47815ba1c05dc4
明輝 陳 <107111210@mail.oit.edu.tw>
2021-03-09 17:04:27 +0800

    判斷閏年

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,23 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int N = 300;
+            if (N % 400 == 0)
+            {
+                Response.Write("Y");
+            }
+            else if (N % 4 == 0 & N % 100 == 0)
+            {
+                Response.Write("N");
+            }
+            else if (N % 4 == 0 & N % 100 != 0)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
