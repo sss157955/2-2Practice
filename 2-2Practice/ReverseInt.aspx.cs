d76823503f752abfdb6156166efdc825329af198
明輝 陳 <107111210@mail.oit.edu.tw>
2021-05-25 21:43:27 +0800

    2-3

---
+++ 2-2Practice/ReverseInt.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_2Practice
+{
+    public partial class ReverseInt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string text = "123456", result = "";
+            foreach (char i in text)
+            {
+
+                result = i + result;
+            }
+            Response.Write(result);
+        }
+    }
+}
